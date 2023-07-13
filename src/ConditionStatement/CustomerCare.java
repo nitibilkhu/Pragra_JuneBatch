a6c1be1c960bd335fcd22974870fb384367a3eb1
Dhavneet <nitibilkhu@gmail.com>
2023-07-13 12:29:00 -0400

    Added files

--- src/ConditionStatement/CustomerCare.java
+++ src/ConditionStatement/CustomerCare.java
@@ -1,2 +1,34 @@
-package ConditionStatement;public class CustomerCare {
-}
+//package ConditionStatement;
+//
+//import java.util.Scanner;
+//
+//public class CustomerCare {
+//    public static void main(String[] args) {
+//        int button; String issue;
+//        System.out.println("Welcome to the Customer Applicatiom");
+//        Scanner sc = new Scanner(System.in);
+//        button = sc.nextInt();
+////        switch(button) {
+////            case 1:
+////                System.out.println("English");
+////                if(issue == "Lost Sim card") {
+////                    System.out.println("lost sim card");
+////                }
+////                else if(issue)
+////                 break;
+////            case 2:
+////                System.out.println("French");
+////                break;
+////            case 3:
+////                System.out.println("Hindi");
+////                break;
+////            default:
+////                System.out.println("Invalid input");
+//
+//
+//        }
+//
+//
+//
+//    }
+//}
